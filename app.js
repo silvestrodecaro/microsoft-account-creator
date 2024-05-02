5ab9439d1cc77f3bb976e28b6deba07286801653
korvinko <dzmitry.kasenka@gmail.com>
2024-05-02 10:07:50 +0300

    Add github account generator

--- app.js
+++ app.js
@@ -65,7 +65,6 @@ async function start() {
 
     await page.waitForNavigation();
     await page.close();
-    await browser.close();
 
     // Writes account's credentials on "accounts.txt".
     const account = `${fullName}@outlook.com` + ":" + `${RandomPassword}!`
